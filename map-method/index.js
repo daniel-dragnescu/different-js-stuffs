ff7a45e07c97d23677d40a3fb6bdeb19cd757409
Dragnescu Daniel <dragnescu_daniel12@yahoo.com>
2024-02-17 14:41:13 +0200

    Map method

---
+++ map-method/index.js
@@ -0,0 +1,45 @@
+// .map() = accepts a callback and applies that function 
+//         to each element of an array, then return a new array
+
+const numbers = [1, 2, 3, 4, 5];
+const squares = numbers.map(square);
+const cubes = numbers.map(cube);
+
+function square(element) {
+  return Math.pow(element, 2);
+}
+function cube(element) {
+  return Math.pow(element, 3);
+}
+
+console.log(squares);
+console.log(cubes);
+
+//-----------------------------------------
+
+const students = ["Doru, Delia, Daniel, Natasa, David"];
+const studentsUpper = students.map(upperCase);
+const studentsLower = students.map(lowerCase);
+
+function upperCase(element) {
+  return element.toUpperCase();
+}
+
+function lowerCase(element) {
+  return element.toLowerCase();
+}
+
+console.log(studentsUpper);
+console.log(studentsLower);
+
+//--------------------------------------------------------
+
+const dates = ["2024-1-10", "2025-2-20", "2026-3-30"];
+const formattedDates = dates.map(formatDates);
+
+function formatDates(element) {
+  const parts = element.split("-");
+  return `${parts[2]}.${parts[1]}.${parts[0]}`;
+}
+
+console.log(formattedDates);
