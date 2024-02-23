7dc526de9db4e16a51b22cafd85191d821ebea40
Dragnescu Daniel <dragnescu_daniel12@yahoo.com>
2024-02-23 17:14:24 +0200

    Type conversion

---
+++ type-conversion/index.js
@@ -0,0 +1,18 @@
+// let age = window.prompt("How old are you?");
+
+// age+= 1;
+
+// console.log(age, typeof age);
+
+
+let x = "0";
+let y = "0";
+let z = "0";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
