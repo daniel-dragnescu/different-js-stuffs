8ed9393cd121a0ddfb81b6abd190cfe751f50e82
Dragnescu Daniel <dragnescu_daniel12@yahoo.com>
2024-02-28 14:34:25 +0200

    Function expressions

---
+++ function-expresions/index.js
@@ -0,0 +1,30 @@
+function hello() {
+  console.log("Hello");
+}
+
+setTimeout(hello, 3000);
+
+//-----------------------------
+
+const numbers = [1, 2, 3, 4, 5, 6];
+const squares = numbers.map((element) => {
+  return Math.pow(element, 2);
+})
+
+const cube = numbers.map((element) => {
+  return Math.pow(element, 3);
+})
+
+const evenNums = numbers.filter((element) => {
+  return element % 2 === 0;
+})
+
+const oddNums = numbers.filter((element) => {
+  return element % 2 !== 0;
+})
+
+const total = numbers.reduce((accumulator, element) => {
+  return accumulator + element;
+})
+
+console.log(total);
