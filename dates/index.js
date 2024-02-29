bddb98174c36bf473e9e617f75d6f62b8f5462e5
Dragnescu Daniel <dragnescu_daniel12@yahoo.com>
2024-02-29 16:54:43 +0200

    Dates

---
+++ dates/index.js
@@ -0,0 +1,42 @@
+// Dates objects = Objects that contain values that represents
+//                 dates and times. These date objects can be
+//                 changed and formatted.
+// Date(year, month, day, hour, minute, seconds, ms)
+
+
+// const date = new Date(2024, 1, 2, 3, 4, 5);
+
+// const date = new Date();
+
+// const year = date.getFullYear();
+// const month = date.getMonth();
+// const day = date.getDate();
+// const hour = date.getHours();
+// const minutes = date.getMinutes();
+// const seconds = date.getSeconds();
+// const dayOfWeek = date.getDay();
+
+// console.log(year);
+// console.log(month);
+// console.log(day);
+// console.log(hour);
+// console.log(minutes);
+// console.log(seconds);
+// console.log(dayOfWeek);
+
+// date.setFullYear(2025);
+// date.setMonth(0);
+// date.setDate(1);
+// date.setHours(2);
+// date.setMinutes(3);
+// date.setSeconds(4);
+
+// console.log(date);
+
+const date1 = new Date("1-11-2024");
+const date2 = new Date("2-11-2024");
+
+if(date2 > date1) {
+  console.log("HAPPY NEW YEAR");
+}
+
