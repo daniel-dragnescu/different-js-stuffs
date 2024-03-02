8e22f3b6b1773df3463ac5c542c90e1b73827d75
Dragnescu Daniel <dragnescu_daniel12@yahoo.com>
2024-03-02 14:24:54 +0200

    Add and change HTML in JS

---
+++ add-change-HTML/index2.js
@@ -0,0 +1,26 @@
+// ---------- EXAMPLE 2 li ELEMENT ----------
+
+// STEP 1 CREATE THE ELEMENT
+const newListItem = document.createElement("li");
+
+// STEP 2 ADD ATTRIBUTES/PROPERTIES
+newListItem.textContent = "coconut";
+newListItem.id = "coconut";
+newListItem.style.fontWeight = "bold";
+newListItem.style.backgroundColor = "lightgreen";
+
+// STEP 3 APPEND ELEMENT TO DOM
+document.body.append(newListItem);
+// document.body.prepend(newListItem);
+// document.getElementById("fruits").append(newListItem);
+// document.getElementById("fruits").prepend(newListItem);
+
+// const banana = document.getElementById("banana");
+// document.getElementById("fruits").insertBefore(newListItem, banana);
+
+// const listItems = document.querySelectorAll("#fruits li");
+// document.getElementById("fruits").insertBefore(newListItem, listItems[1]);
+
+// REMOVE HTML ELEMENT
+// document.body.removeChild(newLink);
+// document.getElementById("fruits").removeChild(newListItem);
